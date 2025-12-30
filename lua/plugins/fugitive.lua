1734aeacdd5e28c9523695dc9c1a6c97ed1dae62
bakalis <bakalis@kth.se>
2025-12-30 09:05:13 +0100

    change git diff to use delta

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -8,10 +8,47 @@ return {
 		-- Git status (like mini lazygit inside Vim)
 		map("n", "<leader>gs", ":Git<CR>", vim.tbl_extend("force", opts, { desc = "Git Status" }))
 
-		-- Diff current file vs HEAD
-		map("n", "<leader>gd", ":Gvdiffsplit<CR>", opts)
+		-- Floating Delta diff preview
+		vim.keymap.set("n", "<leader>gd", function()
+			local file = vim.fn.expand("%:p")
+			local width = math.floor(vim.o.columns * 0.95)
+			local height = math.floor(vim.o.lines * 0.95)
+			local row = math.floor((vim.o.lines - height) / 2)
+			local col = math.floor((vim.o.columns - width) / 2)
 
-		-- Blame current file
+			-- Create a scratch buffer
+			local bufnr = vim.api.nvim_create_buf(false, true) -- [listed = false, scratch = true]
+
+			-- Open floating window
+			local winid = vim.api.nvim_open_win(bufnr, true, {
+				relative = "editor",
+				width = width,
+				height = height,
+				row = row,
+				col = col,
+				style = "minimal",
+				border = "rounded",
+			})
+
+			-- Hide line numbers
+			vim.api.nvim_win_set_option(winid, "number", false)
+			vim.api.nvim_win_set_option(winid, "relativenumber", false)
+
+			-- Open terminal in the floating window running Delta diff
+			vim.fn.termopen("git -c core.pager=delta diff HEAD -- " .. vim.fn.fnameescape(file), {
+				on_exit = function()
+					-- keep buffer open, you close manually
+				end,
+			})
+
+			-- Enter terminal mode
+			vim.cmd("startinsert")
+
+			-- Map 'q' to close the floating window cleanly
+			vim.api.nvim_buf_set_keymap(bufnr, "t", "q", "<C-\\><C-n>:bd!<CR>", { noremap = true, silent = true })
+			vim.api.nvim_buf_set_keymap(bufnr, "n", "q", ":bd!<CR>", { noremap = true, silent = true })
+		end, { desc = "Delta diff (floating current file)" })
+
 		map("n", "<leader>gb", ":Git blame<CR>", opts)
 
 		-- Browse on GitHub (requires vim-rhubarb)
@@ -21,13 +58,5 @@ return {
 
 		-- Open file at last commit
 		map("n", "<leader>gO", ":Gedit HEAD~1:%<CR>", opts)
-
-		vim.keymap.set("n", "<leader>gq", function()
-			for _, win in ipairs(vim.api.nvim_list_wins()) do
-				if vim.wo[win].diff then
-					vim.api.nvim_win_close(win, true)
-				end
-			end
-		end, { noremap = true, silent = true })
 	end,
 }
