d40d2773f928ea29dcd5bb380e3fd89da8de6a7a
bakalis <bakalis@kth.se>
2026-01-08 23:25:49 +0100

    Merge tool (work in progress)

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -9,7 +9,7 @@ return {
 		map("n", "<leader>gs", ":Git<CR>", vim.tbl_extend("force", opts, { desc = "Git Status" }))
 
 		-- Floating Delta diff preview
-		vim.keymap.set("n", "<leader>gd", function()
+		map("n", "<leader>gd", function()
 			local file = vim.fn.expand("%:p")
 			local width = math.floor(vim.o.columns * 0.95)
 			local height = math.floor(vim.o.lines * 0.95)
@@ -48,6 +48,29 @@ return {
 			vim.api.nvim_buf_set_keymap(bufnr, "t", "q", "<C-\\><C-n>:bd!<CR>", { noremap = true, silent = true })
 			vim.api.nvim_buf_set_keymap(bufnr, "n", "q", ":bd!<CR>", { noremap = true, silent = true })
 		end, { desc = "Delta diff (floating current file)" })
+
+		-- Open Git mergetool for the current repo
+		map("n", "<leader>gm", function()
+			-- Open a terminal running `git mergetool`
+			vim.cmd("tabnew") -- optional: open in a new tab
+			local bufnr = vim.api.nvim_get_current_buf()
+			vim.fn.termopen("git mergetool")
+			vim.cmd("startinsert")
+			-- Optional: disable line numbers for terminal
+			vim.api.nvim_buf_set_option(bufnr, "number", false)
+			vim.api.nvim_buf_set_option(bufnr, "relativenumber", false)
+			-- Map 'q' to close the terminal buffer
+			vim.api.nvim_buf_set_keymap(bufnr, "t", "q", "<C-\\><C-n>:bd!<CR>", { noremap = true, silent = true })
+		end, { desc = "Git Mergetool" })
+
+		-- Keep left (ours)
+		map("n", "<leader>ml", ":diffget LO<CR>", opts)
+
+		-- Keep right (theirs)
+		map("n", "<leader>mr", ":diffget RE<CR>", opts)
+
+		-- Keep both
+		map("n", "<leader>mb", ":diffget LO | diffget RE<CR>", opts)
 
 		map("n", "<leader>gb", ":Git blame<CR>", opts)
 
